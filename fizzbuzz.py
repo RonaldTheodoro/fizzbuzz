5c4ce47c491c3c8bf0e8e2db57a0d3b74427bef1
lucas59356 <lucas59356@gmail.com>
2017-11-07 21:24:25 -0200

    Acertar uns detalhes pythonicos e separar código biblioteca de código de teste

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,28 +8,20 @@ Regras do Fizzbuzz
 """
 
 
-import unittest
 from functools import partial
-
 
 multiple_of = lambda base, num: num % base == 0
 multiple_of_5 = partial(multiple_of, 5)
 multiple_of_3 = partial(multiple_of, 3)
 
 
-def robot(pos):
-    say = str(pos)
-    if multiple_of_3(pos) and multiple_of_5(pos):
-        say = 'fizzbuzz'
+def robot(number):
+    if multiple_of_3(number) and multiple_of_5(number):
+        return 'fizzbuzz'
 
-    elif multiple_of_5(pos):
-        say = 'buzz'
-
-    elif multiple_of_3(pos):
-        say = 'fizz'
-
-    return say
-
-
-if __name__ == '__main__':
-    unittest.main()
+    elif multiple_of_5(number):
+        return 'buzz'
+   
+    elif multiple_of_3(number):
+        return 'fizz'
+    return str(number)
